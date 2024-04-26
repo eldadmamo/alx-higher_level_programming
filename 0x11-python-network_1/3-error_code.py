978f4543a8d8abf0bdab4d0e69be978e98cde8a9
Eldad <eldadf456@gmail.com>
2024-04-26 13:19:51 -0700

    3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,21 +1,17 @@
 #!/usr/bin/python3
-"""A script 
-- URL,
-- request to URL
-- displays body response utf-8
+"""A script that:
+- takes in a URL,
+- sends a request to the URL
+- displays the body of the response
 """
-
-import sys
-import urllib.error
-import urllib.request
 
 
 if __name__ == "__main__":
-    url = sys.argv[1]
+    import sys
+    from urllib import request, error
 
-    request = urllib.request.Request(url)
     try:
-        with urllib.request.urlopen(request) as response:
-            print(response.read().decode("ascii"))
-    except urllib.error.HTTPError as e:
-        print("Error code: {}".format(e.code))
+        with request.urlopen(sys.argv[1]) as res:
+            print(res.read().decode('UTF-8'))
+    except error.HTTPError as er:
+        print('Error code:', er.code)
